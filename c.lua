df5569e438e5b2701e16f16225084a6d879de128
geniuN <67476044+geniunn@users.noreply.github.com>
2021-04-30 01:04:05 +0300

    Update c.lua

--- c.lua
+++ c.lua
@@ -1,7 +1,7 @@
 for k in pairs(Config.Triggers) do
-    AddEventHandler(Config.Triggers[k].trigger, function()
-        if Config.Triggers[k].type == "client" then
+    if Config.Triggers[k].type == "client" then
+        AddEventHandler(Config.Triggers[k].trigger, function()
             TriggerServerEvent('geni_triggerLogger:discordLog', GetPlayerServerId(PlayerId()), Config.Triggers[k].title, Config.Triggers[k].message, Config.Triggers[k].webhook, Config.Triggers[k].trigger, Config.Triggers[k].type)
-        end
-    end)
+        end)
+    end
 end
