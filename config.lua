333ab821440cdea3d88cf1e6e8e773536f22b627
geniuN <67476044+geniunn@users.noreply.github.com>
2021-05-03 14:51:51 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,4 +2,5 @@ Config = {}
 Config.Triggers = {
     {trigger = "", type = "client or server", title = "log title", message = "log message", webhook = "log webhook(discord)"},
     {trigger = "", type = "", title = "", message = "", webhook = ""},
+    -- example {trigger = "esx:playerLoaded", type = "client", title = "Spawn", message = "Player spawned.", webhook = ""},
 }
